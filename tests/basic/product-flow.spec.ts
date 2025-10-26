b77ad103132d14bbf174cae971a41e76b59f3299
Sreejaya V S <sreejayatester@gmail.com>
2025-10-26 14:30:17 +0530

    Adding product order flow

--- tests/basic/product-flow.spec.ts
+++ tests/basic/product-flow.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
-import * as products from '../pages/product'
-import * as cart from '../pages/cart'
+import * as products from '../pages/products'
+import * as cart from '../pages/cart';
+import * as checkout from '../pages/checkout'
+import * as contact from '../pages/contact'
 
 test('Item is added to the shopping cart', async ({ page }) => {
     await page.goto('/products');
@@ -15,7 +17,34 @@ test('Item is added to the shopping cart', async ({ page }) => {
 
     expect(subtotal).toBe(addedProduct.price)
 
+})
 
+test('Complete workflow for product order', async ({ page }) => {
+    await page.goto('/products');
+
+    const addedProduct = await products.addProductToCart(page, 1);
+
+    await page.locator('[data-test-id="header-cart-button"]').getByRole('button').click();
+
+    await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
+
+    await checkout.addContactInfo(page)
+    await checkout.addPaymentInfo(page)
+    await checkout.addShippingAddress(page)
+    await checkout.placeOrder(page)
+
+    // get orderId:
+    const orderWrapper = page.getByText('Your Order ID is:').locator('..')
+    const orderId = await orderWrapper.getByRole('paragraph').nth(1).textContent()
+
+    // open the contact page:
+    await page.getByRole('button', { name: 'Track Your Order' }).click();
+    await contact.fillOrderIdAndEmail(page, orderId!, checkout.testValues.email)
+    await contact.clickTrackOrder(page)
+
+    // check if ordered item is returned:
+    const firstOrder = page.getByText(addedProduct.name!)
+    await expect(firstOrder).toBeVisible()
 
 
 })
