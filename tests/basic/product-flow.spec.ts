8c279828372d875d2e7d25d7d0d55467d89e141d
Sreejaya V S <sreejayatester@gmail.com>
2025-10-26 14:33:32 +0530

    Adding step function for clear reporting

--- tests/basic/product-flow.spec.ts
+++ tests/basic/product-flow.spec.ts
@@ -48,3 +48,43 @@ test('Complete workflow for product order', async ({ page }) => {
 
 
 })
+
+test('Complete workflow for product order - with steps', async ({ page }) => {
+    await page.goto('/products');
+
+    let addedProduct: Awaited<ReturnType<typeof products.addProductToCart>> = {} as any;
+
+    await test.step('add product to cart', async () => {
+        addedProduct = await products.addProductToCart(page, 1);
+    })
+
+    await test.step('go to checkout page', async () => {
+        await page.locator('[data-test-id="header-cart-button"]').getByRole('button').click();
+        await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
+    })
+
+    await test.step('complete checkout information', async () => {
+        await checkout.addContactInfo(page)
+        await checkout.addPaymentInfo(page)
+        await checkout.addShippingAddress(page)
+        await checkout.placeOrder(page)
+    })
+
+    let orderId: string | null;
+
+    await test.step('get the orderID', async () => {
+        const orderWrapper = page.getByText('Your Order ID is:').locator('..')
+        orderId = await orderWrapper.getByRole('paragraph').nth(1).textContent()
+    })
+
+    await test.step('open the contact page', async () => {
+        await page.getByRole('button', { name: 'Track Your Order' }).click();
+        await contact.fillOrderIdAndEmail(page, orderId!, checkout.testValues.email)
+        await contact.clickTrackOrder(page)
+    })
+
+    await test.step('check if ordered item is returned', async () => {
+        const firstOrder = page.getByText(addedProduct.name!)
+        await expect(firstOrder).toBeVisible()
+    })
+});
